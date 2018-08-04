1b1beda08a0c33925d60066a5f615515495a5c20
jiwonsim <shark.cametotea@gmail.com>
2018-08-04 18:55:20 +0900

    comment commit

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,17 @@ class Post(models.Model): # Post라는 장고 모델 정의
     #아래 함수를 호출한다면 Post모델의 제목을 얻게 된다.
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
